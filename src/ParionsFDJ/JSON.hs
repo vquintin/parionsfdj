1bb06b6797d1f0fe55d958abfbc55b0ff02e5521
Virgile Quintin <virgile.quintin@gmail.com>
2017-07-29 13:28:58 +0200

    WIP

--- src/ParionsFDJ/JSON.hs
+++ src/ParionsFDJ/JSON.hs
@@ -1,18 +1,44 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module ParionsFDJ.JSON
-  (
+  ( Trend(..)
   ) where
 
-import qualified Data.Time as T
+import qualified Data.Aeson as AE
+import Data.Aeson ((.:))
+import qualified Data.Text as TE
+import qualified Data.Text.Read as TR
+import qualified Data.Time as TI
+
+data Event = Event
+  { eventCompetition :: String
+  , eventCompetitionID :: Int
+  , eventCount :: Int
+  , eventEnd :: Int
+  , eventID :: Int
+  , eventType :: String
+  , formules :: [Formule]
+  , eventSportID :: Int
+  , urlStats :: String
+  } deriving (Eq, Show)
 
 data Formule = Formule
   { competition :: String
   , competitionId :: Int
   , count :: Int
-  , end :: T.UTCTime
+  , end :: TI.UTCTime
+  , formuleEventID :: Int
+  , index :: Int
   , formuleLabel :: String
+  , marketID :: Int
+  , marketType :: String
+  , marketTypeGroup :: String
+  , marketTypeID :: Int
   , outcomes :: [Outcome]
+  , sportID :: Int
   } deriving (Eq, Show)
 
+{- Outcome -}
 data Outcome = Outcome
   { cote :: Double
   , outcomeLabel :: String
@@ -22,8 +48,40 @@ data Outcome = Outcome
   , winner :: Bool
   } deriving (Eq, Show)
 
+instance AE.FromJSON Outcome where
+  parseJSON =
+    AE.withObject "outcome" $ \o ->
+      Outcome <$> (readDoubleWithComma <$> o .: "cote") <*> o .: "label" <*>
+      (readIntAsText <$> o .: "pos") <*>
+      o .: "status" <*>
+      o .: "trend" <*>
+      o .: "winner"
+
+readDoubleWithComma :: TE.Text -> Double
+readDoubleWithComma = read . sanitize . TE.unpack
+  where
+    sanitize =
+      map
+        (\c ->
+           if c == ','
+             then '.'
+             else c)
+
+readIntAsText :: TE.Text -> Int
+readIntAsText = read . TE.unpack
+
+{- Trend -}
 data Trend
   = Down
   | Nil
   | Up
   deriving (Eq, Show)
+
+instance AE.FromJSON Trend where
+  parseJSON =
+    AE.withText "trend" $ \t ->
+      case t of
+        "-1" -> return Down
+        "0" -> return Nil
+        "1" -> return Up
+        _ -> fail $ "Unknown trend " ++ show t
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,41 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Aeson
+import qualified Data.ByteString.Lazy as BS
+import ParionsFDJ.JSON
+import Test.HUnit
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = showCounts <$> runTestTT tests >>= putStrLn
+  where
+    tests = TestList [testParseTrend]
+
+testParseTrend = parseTestWithCases "Error in parsing trend" cases
+  where
+    cases =
+      [ ("\"-1\"", Just Down)
+      , ("\"0\"", Just Nil)
+      , ("\"1\"", Just Up)
+      , ("x", Nothing)
+      ]
+
+parseTest ::
+     (Eq a, FromJSON a, Show a) => String -> BS.ByteString -> Maybe a -> Test
+parseTest preface input expected =
+  TestCase $ assertParsingEqual preface input expected
+
+parseTestWithCases ::
+     (Eq a, FromJSON a, Show a) => String -> [(BS.ByteString, Maybe a)] -> Test
+parseTestWithCases preface cases = TestCase $ foldl f (return ()) cases
+  where
+    f assertion (input, expected) =
+      assertion >> assertParsingEqual preface input expected
+
+assertParsingEqual ::
+     (Eq a, FromJSON a, Show a)
+  => String
+  -> BS.ByteString
+  -> Maybe a
+  -> Assertion
+assertParsingEqual preface input expected =
+  assertEqual preface expected $ decode input
