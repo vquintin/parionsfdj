f0f5eaf3859cfc9114214bb93f69e3a06d37da56
Virgile Quintin <virgile.quintin@gmail.com>
2017-08-29 16:31:09 +0200

    WIP

--- src/ParionsFDJ/Parse.hs
+++ src/ParionsFDJ/Parse.hs
@@ -3,13 +3,16 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 
 module ParionsFDJ.Parse
-  (
+  ( parseEvent
   ) where
 
 import qualified HBet.BetSum as HBS
 import qualified ParionsFDJ.JSON.Event as EV
 import ParionsFDJ.Parse.Football
 import ParionsFDJ.Parse.Parsable
+
+parseEvent :: EV.Event -> Either String [HBS.ChoiceSum ()]
+parseEvent = parseData
 
 instance Parsable EV.Event [HBS.ChoiceSum ()] where
   parseData ev =
